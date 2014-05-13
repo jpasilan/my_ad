a1cae2f747b3ed99f78e2fb06d753df35ee7de17
Josel Pasilan <jpasilan@gmail.com>
2014-05-14 04:57:24 +0800

    #2 Added the Sentry package and written the initial User controller class.

---
+++ app/controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+class UserController extends BaseController
+{
+
+    /**
+     * User registration
+     *
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Password reset
+     *
+     */
+    public function resetPassword()
+    {
+
+    }
+
+    /**
+     * Password reset request
+     *
+     */
+    public function requestPassword()
+    {
+
+    }
+
+} 
