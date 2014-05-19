8ed9ccd9abee80816c663e81a65ccedb2c02064e
vpanugaling <vincent.panugaling@tekvitals.com>
2014-05-19 20:56:10 +0800

    #1: Added docblock comments to StaticPagesController methods

--- app/controllers/StaticPagesController.php
+++ app/controllers/StaticPagesController.php
@@ -2,27 +2,52 @@
 
 class StaticPagesController extends BaseController
 {
-
+    
+    /**
+     * Static Homepage
+     * 
+     * @return mixed
+     */
     public function index()
     {
         return View::make('static.home');
     }
     
+    /**
+     * Static OurStory Page
+     * 
+     * @return mixed
+     */
     public function ourStory()
     {
         return View::make('static.our-story');
     }
     
+    /**
+     * Static About Us Page
+     * 
+     * @return mixed
+     */
     public function aboutUs()
     {
         return View::make('static.about-us');
     }
     
+    /**
+     * Static Privacy Policy Page
+     * 
+     * @return mixed
+     */
     public function privacyPolicy()
     {
         return View::make('static.privacy-policy');
     }
     
+    /**
+     * Static Terms and Condition Page
+     * 
+     * @return mixed
+     */
     public function termsAndCondition()
     {
         return View::make('static.terms-and-condition');
